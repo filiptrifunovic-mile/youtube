a6200696ea7770ddd7370fe00a29bf2c55db3347
Filip <filip.trifunovic23@gmail.com>
2022-12-24 11:58:10 +0100

    creating utils

---
+++ src/utils/constants.js
@@ -0,0 +1,45 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "JS Mastery", icon: <CodeIcon /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "NextJS", icon: <CodeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl =
+  "https://www.patterns.dev/img/reactjs/react-logo@3x.svg";
+export const demoChannelUrl = "/@freecodecamp";
+export const demoVideoUrl = "/watch?v=bMknfKXIFA8";
+export const demoChannelTitle = "Learn React";
+export const demoVideoTitle =
+  "React Course - Beginner's Tutorial for React JavaScript Library [2022]";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
