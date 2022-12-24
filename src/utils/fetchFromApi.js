21f96ad2b2a6a9b68091f1d59da8925147699e4e
Filip <filip.trifunovic23@gmail.com>
2022-12-24 14:12:38 +0100

    fetching data from api

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "692d6f6bd3msh35ed7227c407e94p1f084bjsn2d17ce0691ec",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
