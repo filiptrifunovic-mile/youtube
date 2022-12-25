0e2fcd2773e292e7295bf54be3d43d5ff199b0a7
Filip <filip.trifunovic23@gmail.com>
2022-12-25 13:27:57 +0100

    display videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import ChannelCard from "./ChannelCard";
+import VideoCard from "./VideoCard";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
-  return <div>Videos</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item?.id?.videoId && <VideoCard video={item} />}
+          {/* {item?.id?.channelId && <ChannelCard channelDetail={item} />} */}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
